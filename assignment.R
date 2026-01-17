f24cea6db4187ab2c4de223fbd272dc845edd0b6
locusclassicus <alieva.mgl@gmail.com>
2026-01-17 08:15:58 +0300

    initial commit

--- assignment.R
+++ assignment.R
@@ -1,76 +1,93 @@
-# В архиве letters вы найдете письма Льва Толстого в формате XML.
-# Вам надо клонировать репозиторий с дз, выполнить задание и закоммитить изменения.
-# Не меняйте структуру репозитория. Не переименовывайте файл с заданием.
-# Не переименовывайте переменные. 
-
-# Вам надо извлечь из каждого файла том, дату и адресата, и собрать эти данные в одну таблицу. 
-# дата письма: в header — тег correspAction, тип sending — тег date, атрибут when;
-# адресат: в header – тег correspAction, тип receiving — имя получателя (текст)
-# том: в header — biblScope, юнит vol — номер тома
-
-# Применяйте trimws() к результату парсинга, чтобы избавиться от лишних строк. 
-
-library(xml2)
-library(dplyr)
-library(purrr)
-
-unzip("letters.zip")
-
-my_xmls <- list.files("letters/", full.names = TRUE)
-
-# пишем код для одного письма
-test_xml <- my_xmls[1]
-doc <- read_xml(test_xml)
-ns <- xml_ns(doc)
-
-# дата письма
-date <- xml_find_first(doc, "//d1:correspAction[@type='sending']//d1:date", ns) |> 
-  xml_attr("when") 
+# установите и загрузите пакеты
+library(friends)
+library(tidyverse)
+library(tidytext)
+library(factoextra)
 
 
+# 1. отберите 6 главных персонажей (по количеству реплик)
+# сохраните как символьный вектор
+top_speakers <- friends |> 
+  count(speaker) |> 
+  arrange(-n) |> 
+  slice_head(n = 6) |> 
+  pull(speaker)
 
-# адресат письма
-corresp <- xml_find_first(doc, "//d1:correspAction[@type='receiving']//d1:persName", ns) |> 
-  xml_text() |> 
-  trimws()
+# 2. токенизируйте текст, удалите цифры
+# стоп-слова не удаляйте!
+friends_tokens <- friends |> 
+  filter(speaker %in% top_speakers) |> 
+  unnest_tokens(word, text) |> 
+  filter(!str_detect(word, "\\d+")) |> 
+  select(speaker, word) 
+
+# 3. отберите по 500 самых частотных слов для каждого персонажа
+# посчитайте относительные частотности для слов
+friends_tf <- friends_tokens |>
+  count(speaker, word) |> 
+  group_by(speaker) |> 
+  arrange(-n) |> 
+  slice_head(n = 500) |> 
+  ungroup() |> 
+  mutate(total = sum(n), .by = speaker) |> 
+  mutate(tf = n/total) |> 
+  select(-n, -total) 
+
+# 4. преобразуйте в широкий формат; 
+# столбец c именем спикера превратите в имя ряда, используя подходящую функцию 
+friends_tf_wide <- friends_tf |> 
+  pivot_wider(names_from = word, 
+              values_from = tf, values_fill = 0) |> 
+  column_to_rownames("speaker")
+
+# 5. установите зерно 123
+# проведите кластеризацию k-means (k = 3) на нормализованных данных
+set.seed(123)
+km.out <- kmeans(scale(friends_tf_wide), centers = 3, nstart = 20)
+
+km.out$cluster
+
+# 6. примените к матрице метод главных компонент (prcomp)
+# центрируйте и стандартизируйте, использовав аргументы функции
+pca_fit <- prcomp(friends_tf_wide, scale = TRUE, center = TRUE)
+
+# 7. Покажите наблюдения и переменные вместе (биплот)
+# в качестве геома используйте текст (=имя персонажа)
+# цветом закодируйте кластер, выделенный при помощи k-means
+# отберите 20 наиболее значимых переменных (по косинусу, см. документацию к функции)
+# сохраните график как переменную q
+
+q <- fviz_pca_biplot(pca_fit,  geom = c("text"),
+                     select.var = list(cos2 = 20),
+                     habillage = as.factor(km.out$cluster)) 
 
 
 
-# том 
+# дополнительно попробуйте добиться оформления примерно как на фото в задании (не оценивается)
+# весь текст шрифтом "Permanent Marker" от Google Fonts 
+# вместо geom_text - geom_изображение (есть в папке) -
+# для этого передайте geom_image() координаты каждого персонажа
+# тема theme_friends() из ThemePark
 
-vol <- xml_find_first(doc, "//d1:biblScope[@unit='vol']", ns) |> 
-  xml_text() |> 
-  trimws()
+library(showtext)
+font_add_google("Permanent Marker", family = "friends")
+showtext_auto()
 
+# install.packages("remotes")
+#remotes::install_github("MatthewBJane/ThemePark")
+library(ThemePark)
+library(ggimage)
 
-### ---------------------------###
-## теперь оборачиваем в функцию
-read_letter <- function(xml_path) {
-  doc <- read_xml(xml_path)
-  ns <- xml_ns(doc)
-  
-  # дата письма
-  date <- xml_find_first(doc, "//d1:correspAction[@type='sending']//d1:date", ns) |> 
-    xml_attr("when") 
-  
-  # адресат письма
-  corresp <- xml_find_first(doc, "//d1:correspAction[@type='receiving']//d1:persName", ns) |> 
-    xml_text() |> 
-    trimws()
-  
-  vol <- xml_find_first(doc, "//d1:biblScope[@unit='vol']", ns) |> 
-    xml_text() |> 
-    trimws()
-  
-  # записываем в тиббл
-  res <- tibble(
-    date = date,
-    corresp = corresp,
-    vol = vol
-  )
-  
-  return(res)
-}
+pca_coords <- as.data.frame(pca_fit$x) |> 
+  mutate(image_file = paste0("friends_images/", rownames(friends_tf), ".jpg"))
 
+fviz_pca_biplot(pca_fit,  geom = c("image"),
+                select.var = list(cos2 = 20),
+                col.var = "steelblue",
+                alpha.var = 0.3,
+                repel = TRUE,
+                ggtheme = theme_friends(),
+                font.family = "friends") +
+  theme(legend.position = "none") +
+  geom_image(data = pca_coords, aes(x = PC1, y = PC2, image = image_file), size = 0.1) 
 
-letters_tbl <- map_dfr(my_xmls, read_letter)
